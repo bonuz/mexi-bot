d195ec4cc094b669bb806f01c69d4f2523a925de
FNC <fiacka@gmail.com>
2021-05-20 21:16:27 -0300

    Some updates

--- index.js
+++ index.js
@@ -3,29 +3,27 @@ const config = require("./config.json");
 const ytdl = require('ytdl-core');
 
 const client = new Discord.Client();
-
 const prefix = "!";
-
+const channelCode = "608447650316156939";
+const mexiId = "608455853539852289";
 client.login(config.BOT_TOKEN);
 
 /**
  * The ready event is vital, it means that only _after_ this will your bot start reacting to information
  * received from Discord
  */
- client.on('ready', () => {
+
+client.on('ready', () => {
     console.log('I am ready!');
-  });
+});
 
-
-
-  client.on("ready", () => {
-    const channel = client.channels.cache.get("843349673716219958");
+client.on("ready", () => {
+    const channel = client.channels.cache.get(channelCode);
     if (!channel) return console.error("The channel does not exist!");
     channel.join().then(connection => {
         // Yay, it worked!
         console.log("Successfully connected.");
     }).catch(e => {
-
         // Oh no, it errored! Let's log it to console :)
         console.error(e);
     });
@@ -34,27 +32,19 @@ client.login(config.BOT_TOKEN);
 client.on('voiceStateUpdate', (oldState, newState) =>{
     // check for bot
     if (oldState.member.user.bot) return;
-    if (newState.channelID === null) console.log('user left channel', oldState.channelID);
-    else if (oldState.channelID === null) console.log('user joined channel', newState.channelID);
-    else console.log('user moved channels', oldState.channelID, newState.channelID);
+    // if (newState.channelID === null) console.log('user left channel', oldState.channelID);
+    // else if (oldState.channelID === null) console.log('user joined channel', newState.channelID);
+    // else console.log('user moved channels', oldState.channelID, newState.channelID);
 
-    console.log(newState);
+    console.log('id: ', newState.id);
+    //console.log(newState);
     
-    const broadcast = client.voice.createBroadcast();
-    broadcast.play('amateur.mp3');
-    //broadcast.play(ytdl('https://www.youtube.com/watch?v=Pke3A2XuBLg', { filter: 'audioonly', }));
-    // Play "music.mp3" in all voice connections that the client is in
-    for (const connection of client.voice.connections.values()) {
-        connection.play(broadcast);
+    if (newState.channelID === channelCode && newState.id === mexiId) {
+        Amateur();
     }
 });
 
 client.on("message", function(message){
-    // if (message.content === 'what is my avatar') {
-    //     // Send the user's avatar URL
-    //     message.reply(message.author.displayAvatarURL());
-    //   }
-
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
 
@@ -71,5 +61,17 @@ client.on("message", function(message){
         const sum = numArgs.reduce((counter, x) => counter += x);
         message.reply(`The sum of all the arguments you provided is ${sum}!`);
     }
+    else if (command === "mexican") {
+        Amateur();
+    }
 });
 
+function Amateur(){
+    const broadcast = client.voice.createBroadcast();
+    broadcast.play('amateur.mp3');
+    //broadcast.play(ytdl('https://www.youtube.com/watch?v=Pke3A2XuBLg', { filter: 'audioonly', }));
+    // Play "music.mp3" in all voice connections that the client is in
+    for (const connection of client.voice.connections.values()) {
+        connection.play(broadcast);
+    }
+}
